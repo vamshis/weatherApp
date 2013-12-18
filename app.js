88ee95be6edf9f0d851e71bb41cf3b3d08f71228
vamshis <vamshi@shippable.com>
2013-12-18 21:55:29 +0000

    Basic function done

--- app.js
+++ app.js
@@ -29,10 +29,21 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//app.get('/', routes.index);
-//request.get({url:'http://www.myweather2.com/developer/forecast.ashx?uac=pNuxVGYDai&query=44011&temp_unit=f&ws_unit=kph', json:true}, function (err, res, data) {
-//      console.log(data);
-//});
+app.get('/', routes.index);
+
+app.post('/', function(req, res){
+   var zipcode = req.body.zip;
+   var Url = "http://www.myweather2.com/developer/forecast.ashx?uac=pNuxVGYDai&output=json&query=" + zipcode + "&temp_unit=f&ws_unit=kph";
+   Url = Url.toString();
+
+   var weatherData;
+   var response = res;
+
+   request.get({url:Url}, function (err, res, data) {
+     weatherData = JSON.parse(data);
+     routes.weather(req, response, weatherData);
+   });
+});
 
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,8 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.weather = function(req, res, data){
+//  console.log(data);
+  res.render('weather', {weatherData:data});
+};
